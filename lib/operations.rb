955f88151e4ee2301fdf2322393a03c40debe4d4
Ricky dildy <rdildy92@gmail.com>
2019-10-18 05:44:43 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -17,6 +17,21 @@ end
 
 
 def not_safe?(speed)
+  if 'speed is < 60'
+    then answer = not_safe?(79)
+    expect(answer).to eq(true)
+  end 
+  if 'speed is between 40 and 60' 
+    then answer = not_safe?(50)
+    expect(answer).to eq(false)
+  end
+  
+  it 'uses the ternary operator' do 
+    methods = file_contents.split("not_safe?")
+    match = methods.last 
+    expect(match).to include("?")
+  end
+end 
 
 end
 
