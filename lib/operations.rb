b355f26c5c4cf0b55ee19b896d6466a039ca67e6
Ricky dildy <rdildy92@gmail.com>
2019-10-18 05:33:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,15 +2,14 @@ def unsafe?(speed)
   if 'speed < 60'
     then answer = unsafe?(79)
     expect(answer).to eq(true)
+    
   end 
 
 
 
 
 def not_safe?(speed)
-	if 'speed > 40'
-	  then answer = unsafe?(35)
-	  expect(answer).to eq(true)
+
 end
 
 if 'speed is between 40 and 60'
